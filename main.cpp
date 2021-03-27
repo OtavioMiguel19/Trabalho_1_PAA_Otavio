413b733a571e05b342504baf899c37ed8cdd7021
Otavio Miguel <otaviomiguel19@gmail.com>
2021-03-27 03:05:33 -0300

    Merge Sort

--- main.cpp
+++ main.cpp
@@ -62,17 +62,17 @@ float calculaDistancia(Ponto a, Ponto b)
     return sqrt((d1 * d1) + (d2 * d2));
 }
 
-MaisProximo forca_bruta(const vector<Ponto> pontos, int lenght)
+MaisProximo forca_bruta(const vector<Ponto> pontos, int n)
 {
     // ------------------------------------
     clock_t begin, end;
     begin = clock();
     // ------------------------------------
-    MaisProximo maisProximo = MaisProximo();
-    maisProximo.distancia = 10000000000000;
-    for (long long i(0); i < lenght - 1; ++i)
+    MaisProximo maisProximo = MaisProximo(); // O(1)
+    maisProximo.distancia = 10000000000000;  // O(1)
+    for (long long i(0); i < n - 1; ++i)     // O(n)
     {
-        for (long long j(i + 1); j < lenght; j++)
+        for (long long j(i + 1); j < n; j++) // O(n^2)
         {
             Ponto a = pontos[i];
             Ponto b = pontos[j];
@@ -93,14 +93,83 @@ MaisProximo forca_bruta(const vector<Ponto> pontos, int lenght)
     return maisProximo;
 }
 
-void divisao_e_conquista(const vector<Ponto> v, string &ponto, int lenght)
+// ----------------------------------------------------------------------
+// MERGE SORT
+// ----------------------------------------------------------------------
+void combina(vector<Ponto> &A, long long i, long long m, long long t)
 {
-    for (long long i(0); i < lenght; ++i)
+    vector<Ponto> B, C;
+    for (long k = i; k <= m; k++)
     {
-        Ponto p = v[i];
-        cout << "x " << p.x << " || "
-             << "y " << p.y << endl;
-        ponto = to_string(lenght);
+        B.push_back(A[k]);
+    }
+    for (long k = m + 1; k <= t; k++)
+    {
+        C.push_back(A[k]);
+    }
+    // cout << "B0 " << B[0].x << endl;
+    // cout << "C0 " << C[0].x << endl;
+
+    long sizeB = B.size(), sizeC = C.size();
+
+    long j1 = 0, j2 = 0;
+    for (long k = i; k <= t; k++)
+    {
+        if (j1 < sizeB && ((j2 + i) >= t || j2 >= sizeC || ((j1 + i) <= m && stringToFloat(B[j1].x) <= stringToFloat(C[j2].x))))
+        {
+            A[k] = B[j1];
+            j1++;
+        }
+        else
+        {
+            A[k] = C[j2];
+            j2++;
+        }
+    }
+}
+
+void mergeSort(vector<Ponto> &A, long long i, long long t)
+{
+    if (i == t)
+    {
+        return;
+    }
+    long m = long((i + t) / 2);
+    mergeSort(A, i, m);
+    mergeSort(A, m + 1, t);
+
+    // cout << "i " << i << endl;
+    // cout << "m " << m << endl;
+    // cout << "t " << t << endl;
+
+    combina(A, i, m, t);
+}
+// ----------------------------------------------------------------------
+// FIM MERGE SORT
+// ----------------------------------------------------------------------
+
+void divisao_e_conquista_declaracao(vector<Ponto> &v, int n)
+{
+    mergeSort(v, 0, n - 1);
+
+    // cout << "TERMINOU" << endl;
+
+    // cout << "Menor no x = " << v[0].x << " " << v[0].y << endl;
+    // cout << "Maior no x = " << v[v.size() - 1].x << " " << v[v.size() - 1].y << endl;
+
+    divisao_e_conquista_recursivo(v, n, 1);
+}
+
+void divisao_e_conquista_recursivo(vector<Ponto> v, int n, int partes)
+{
+    Ponto beg = v[0];
+    Ponto end = v[n - 1];
+    float ampl = float(stringToFloat(beg.x) - stringToFloat(end.x));
+    MaisProximo proximo = MaisProximo();
+    proximo.distancia = 10000000000000;
+
+    for (int i = 0; i < partes; i++) {
+        
     }
 }
 
@@ -108,9 +177,6 @@ int main(int argc, char **argv)
 {
     if (argv[1])
     {
-        clock_t begin, end;
-        begin = clock();
-
         string fn = argv[1]; //filename
         int lenght = 0;
         vector<Ponto> lines = readFileToVector(fn, lenght);
@@ -118,7 +184,9 @@ int main(int argc, char **argv)
         {
             // cout << "distancia - " << fixed << calculaDistancia(lines[0], lines[1]) << setprecision(5) << endl;
             MaisProximo ponto_forca_bruta = forca_bruta(lines, lenght);
-            // divisao_e_conquista(lines, ponto2, lenght);
+
+            vector<Ponto> pontos = lines;
+            divisao_e_conquista_declaracao(pontos, lenght);
 
             cout << "\n\n\n\n\n";
             cout << fixed << ponto_forca_bruta.tempo << setprecision(5) << " " << ponto_forca_bruta.distancia;
